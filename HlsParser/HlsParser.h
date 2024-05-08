0969aa1f2a3d2619334313a4f6ecb2a9255dacff
Daniel Pinto <dpinto@stevens.edu>
2024-05-07 21:04:08 -0400

    Add in Parser Interface.

--- HlsParser/HlsParser.h
+++ HlsParser/HlsParser.h
@@ -9,11 +9,11 @@
 #include <vector>
 #include "ParsedData.h"
 
-class HlsParser {
+class HlsParser : Parser {
 public:
 	HlsParser();
 	~HlsParser();
-	void Parse(const LPCWSTR& fileLocation, ParsedData&);
+	void Parse(const LPCWSTR& fileLocation, ParsedData& parsedData);
 	std::unique_ptr<VideoType> ParseVideoLine(const std::string &line, const std::string &newLine);
 	std::unique_ptr<ImageType> ParseImageLine(const std::string &line);
 	std::unique_ptr<AudioType> ParseAudioLine(const std::string &line);
--- HlsParser/Parser.h
+++ HlsParser/Parser.h
@@ -1,8 +1,8 @@
 #ifndef __PARSER__
 #define __PARSER__
-
+#include "ParsedData.h"
 class Parser {
 	// Pure virtual function to overwrite for our specific parser
-	//virtual void Parse(LPCWSTR fileLocation, ParsedData&) = 0;
+	virtual void Parse(const LPCWSTR& fileLocation, ParsedData& parsedData) = 0;
 };
 #endif
